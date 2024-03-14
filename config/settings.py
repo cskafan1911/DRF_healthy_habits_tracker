887de1a01b4c10767a074336b99200a0d9fe94ed
Dmitry Dronov <cskafan1911@gmail.com>
2024-03-14 21:06:52 +0300

    Настройки CORS

--- config/settings.py
+++ config/settings.py
@@ -199,12 +199,13 @@ SWAGGER_SETTINGS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "https://read-only.example.com",
-    "https://read-and-write.example.com",
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://read-and-write.example.com",
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
