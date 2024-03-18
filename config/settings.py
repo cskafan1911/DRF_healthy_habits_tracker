ec9a692707b55ee810275539fcb1fbdf635343f6
Dmitry Dronov <cskafan1911@gmail.com>
2024-03-18 21:13:45 +0300

    настройки для docker-compose

--- config/settings.py
+++ config/settings.py
@@ -214,10 +214,10 @@ CORS_ALLOW_ALL_ORIGINS = False
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_BOT')
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_BROKER_URL = f'redis://redis:6379'
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = f'redis://redis:6379'
 
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = "Europe/Moscow"
